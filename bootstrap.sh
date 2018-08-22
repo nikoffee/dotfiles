5de0ae3cb8861eacddccdf017964df1c45a878c9
Greg Houle <greg.houle@shopify.com>
2018-08-22 10:24:00 -0400

    added initial brew setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,17 @@
 # Setup file for dotenv
 
+cur_dir=$(pwd)
 dir=~/dotfiles
 old_dir=~/dotfiles_old
+brew_dump=$dir/startup/brew
 files=".bash_profile .bashrc .gitconfig .pryrc .vimrc .zshrc"
+
+echo "Initial Brew setup"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+cd $brew_dump
+brew bundle
+cd $cur_dir
+echo "...\nDone\n"
 
 nvim_dir=~/.config/nvim
 nvim_files="init.vim"
@@ -15,14 +24,18 @@ echo "...\nDone"
 echo "Changing to $dir"
 cd $dir
 
+echo "Move old dotfiles and link new ones..."
 for file in $files; do
   mv ~/$file $old_dir/ 2>/dev/null
   ln -sfn $dir/$file ~/$file
 done
+echo "...\nDone"
 
+echo "Move old nvim files and link new ones"
 for file in $nvim_files; do
   mv $nvim_dir/$file $old_dir/ 2>/dev/null
   ln -sfn $dir/$file $nvim_dir/$file
 done
 
 ln -sfn $dir/snippets/* $nvim_snippets/
+echo "...\nDone"
