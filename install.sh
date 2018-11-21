2c2732b14ef9eadd4b0077f19add3a94a70afcda
Greg Houle <42624241+nikoffee@users.noreply.github.com>
2018-11-20 20:57:20 -0500

    Update install.sh
    added xcode-select install

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Trying to install xcode clt to trigger the interstitial (it's a noop if already installed)"
+xcode-select --install
+
 if [ ! -d "$HOME/dotfiles" ]; then
   echo "Installing your dotfiles"
   git clone https://github.com/nikoffee/dotfiles.git "$HOME/dotfiles"
