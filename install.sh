d0e769891a836e564010f6b448706fbd3e1dda34
Greg Houle <42624241+nikoffee@users.noreply.github.com>
2018-11-20 20:53:23 -0500

    Update install.sh
    add mkdir if not there

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 if [ ! -d "$HOME/dotfiles" ]; then
+  mkdir -p $HOME/dotfiles
   echo "Installing your dotfiles"
   git clone https://github.com/nikoffee/dotfiles.git "$HOME/dotfiles"
   cd "$HOME/dotfiles"
